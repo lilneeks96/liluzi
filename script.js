66386c137a76637e543bb2e0122c1fbfb6c168f3
Nicolas Dela Cruz <ndelacruz@MacBook-Air.home>
2022-08-19 16:36:43 -0700

    more docs

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ let arr= [];
 
 
 // taking user input and pushing it into array
+// challenges below! now find a way to take user input and remove from array 
+// also show array on current page? than on another part of the website?
+// make multiple buttons with different ways to sort the array
+// sort array so most characters are left and least characters are right
+
 document.getElementById("myButton").onclick = function(){
 
     var myName = document.getElementById("myText");
