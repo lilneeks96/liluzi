658f5688bd34e41f607d4df53b49cf8276b42dcb
Nicolas Dela Cruz <ndelacruz@MacBook-Air.home>
2022-08-19 16:21:58 -0700

    warming up baby, slowly getting better.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function testFunction(a){
-    a = "real end of script!!";
-    console.log('a: ', a);
-    return a;
-}
-testFunction();
+//EASY WAY
+//var myName = window.prompt("What's your name?");
+//console.log("Hello",myName);
+let addArray= [];
 
+//HARD WAY
+document.getElementById("myButton").onclick = function(){
+
+    var myName = document.getElementById("myText");
+    addArray.push(myName.value)
+    console.log(addArray);
+};
